70ffaa4ebcbbd5a20f1d9f33b11c9a6f8c615450
Robert Brown <robertlb4@gmail.com>
2018-04-08 18:19:19 -0700

    finish handling for submitting edited item

--- shopping-list-redux/index.js
+++ shopping-list-redux/index.js
@@ -17,6 +17,7 @@ function generateItemElement(item, itemIndex) {
   return `
     <li class="js-item-index-element" data-item-index="${itemIndex}">
       <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
+      <a class="editButton" href="">edit</a>
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle js-item-toggle">
             <span class="button-label">check</span>
@@ -104,13 +105,35 @@ function handleDisplayCheckedItems() {
 }
 
 function handleSearchInput() {
-  //let search = '';
   $('.itemSearch').keyup( e	 => {
-    //search = $(e.target).val();
     STORE.filterby = $(e.target).val();
     renderShoppingList();
   });
 }
+
+function itemEditHandler () {
+  let placeHolder;
+  let itemIndex;
+  $('.js-shopping-list').on('click', '.editButton', e => {
+    itemIndex = getItemIndexFromElement(e.currentTarget);
+    e.preventDefault();
+		
+    placeHolder = $(e.target).siblings('span').text();
+		
+    $(e.target).siblings('span').replaceWith(`<input type= "text"
+		class="editItem" value= ${placeHolder}>`);
+  });
+	
+  $('.js-shopping-list').keyup( e => {
+    if (e.which === 13) {
+      if ($(e.target).val() === placeHolder) renderShoppingList();
+      else STORE.items[itemIndex].name = $(e.target).val();
+      renderShoppingList();    
+    }
+    
+  });
+}
+
 
 function handleShoppingList() {
   renderShoppingList();
@@ -119,6 +142,7 @@ function handleShoppingList() {
   handleDeleteItemClicked();
   handleDisplayCheckedItems();
   handleSearchInput();
+  itemEditHandler();
 }
 
 $(handleShoppingList);
